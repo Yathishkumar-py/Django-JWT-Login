28dc4f919cd64e4e86255d5bcb24eafaae175c9e
Yathish-Softsuave <89400066+Yathish-Softsuave@users.noreply.github.com>
2021-08-23 20:20:16 +0530

    Update settings.py

--- django_projects/django_JWT_Auth/django_JWT_Auth/settings.py
+++ django_projects/django_JWT_Auth/django_JWT_Auth/settings.py
@@ -142,5 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'iamyathi01@gmail.com'
-EMAIL_HOST_PASSWORD = 'sbuurtzucujplljb'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
