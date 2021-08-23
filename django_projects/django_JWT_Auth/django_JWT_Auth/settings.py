c048f8ad0b49315e1b0992be6c44ae9c5c65e0ee
Yathish-Softsuave <yathishkumar.satheesh@softsuave.com>
2021-08-23 22:15:15 +0530

    initial commit

--- django_projects/django_JWT_Auth/django_JWT_Auth/settings.py
+++ django_projects/django_JWT_Auth/django_JWT_Auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-29^-dnve23cusxr_dy$ii(&td31n(21y=owzo(om)xsm4l9fuu'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
